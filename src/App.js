136cddf8e2f8d46fc1eac7d5cd780e33623c1171
hafiz <hafizn24@gmail.com>
2022-10-24 11:08:07 +0800

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { createTheme, ThemeProvider} from '@mui/material/styles'
+import { CssBaseline } from '@mui/material'
+
+import Main from './Main'
 
 function App() {
+  const font = "'Alegreya', serif"
+  const theme = createTheme({
+    // palette: {
+    //   type: 'light',
+    //   primary: {
+    //     main: '#DD5353',//#ff869e
+    //     light: '#EDDBC0',//#ffc4c4
+    //     dark: '#B73E3E',//#a10035
+    //   },
+    //   secondary: {
+    //     main: '#DBC8AC',//#FFE7BF
+    //   },
+    // },
+    typography: {
+      fontFamily: font,
+    },
+  });
+
+// ####################CORS####################
+  // var express = require('express')
+  // var cors = require('cors')
+  // var app = express()
+  
+  // app.use(cors())
+  
+  // app.get('/products/:id', function (req, res, next) {
+  //   res.json({msg: 'This is CORS-enabled for all origins!'})
+  // })
+  
+  // app.listen(80, function () {
+  //   console.log('CORS-enabled web server listening on port 80')
+  // })
+// ####################CORS####################
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline enableColorScheme />
+      <Main />
+    </ThemeProvider>
   );
 }
 
---
+++ src/Main.js
@@ -0,0 +1,57 @@
+import { useEffect, useState } from 'react'
+import { Typography } from '@mui/material/'
+import Axios from 'axios'
+
+function Main() {
+    const [mongo, setMongo] = useState('')
+    let id = parseInt(1)
+
+    let data = JSON.stringify({
+        "collection": "character",
+        "database": "hafiz-db",
+        "dataSource": "Cluster0",
+        "filter": {
+            "id": id
+        }
+    })
+
+    let config = {
+        method: 'post',
+        url: '/findOne',
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Request-Headers': '*',
+          'api-key': 'h2j4wkjpoEPuVndrEuMHnVVAukwAH5ySDgI80DeD8Ig5SNEKm0cRkXuCvBjSOCNS',
+        },
+        data: data
+    };
+    
+    useEffect(() => {
+        Axios(config)
+        .then(function (response) {
+            console.log(response);
+            setMongo(response.data.document.name)
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+    }, [])
+    return (
+        <>
+            <Typography
+                align='center'
+                variant='h3'
+            >
+                Text-RPG!
+            </Typography>
+            <Typography
+                align='center'
+                variant='subtitle1'
+            >
+                {mongo}
+            </Typography>
+        </>
+    )
+}
+
+export default Main
